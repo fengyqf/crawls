7ec3bb96ce922ef975c682987800ea176221b35a
fengyqf <fengyqf@gmail.com>
2018-09-07 14:44:45 +0800

    crawl h-index from google scholar

---
+++ crawl_h_index/config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+debug=1
+
+
+browser_driver_command_executor='http://192.168.7.113:4444/wd/hub'
+browser_profile_dir='/cygdrive/f/tempdown/crawl_firefox_profile/8jokogoy.default-1536211405626'
+
+start_url='https://scholar.google.com/citations?view_op=top_venues&hl=en&vq=en'
+storage_subdir='data'
+backup_subdir='backup'
+
+
+proxy_type = "socks"
+proxy_host = "192.168.7.100"
+proxy_port = "1080"
+
+
